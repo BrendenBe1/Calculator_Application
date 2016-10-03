3b5678303271366b7cc944a4cafbb5291db99927
BrendenBe1 <brendenbe1@gmail.com>
2016-10-02 19:59:15 -0700

    calculator working mostly. need to fix the spacing issue

--- app/src/main/java/brendenbernal/basiccalculator/MainActivity.java
+++ app/src/main/java/brendenbernal/basiccalculator/MainActivity.java
@@ -2,11 +2,16 @@ package brendenbernal.basiccalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,13 +44,16 @@ public class MainActivity extends AppCompatActivity {
        Button buttonMULT = (Button) findViewById(R.id.buttonMULT);
        Button buttonDIV = (Button) findViewById(R.id.buttonDIV);
        Button buttonDEC = (Button) findViewById(R.id.buttonDEC);
+       Button buttonOPARENTH = (Button) findViewById(R.id.buttonOPARENTH);
+       Button buttonCPARENTH = (Button) findViewById(R.id.buttonCPARENTH);
+       Button buttonEQ = (Button) findViewById(R.id.buttonEQ);
 
        button0.setOnClickListener(new OnClickListener()
        {
            @Override
            public void onClick (View v)
            {
-               screenText.append("0");
+               screenText.append("0 ");
            }
        });
 
@@ -54,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("1");
+               screenText.append("1 ");
            }
        });
 
@@ -63,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("2");
+               screenText.append("2 ");
            }
        });
 
@@ -72,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("3");
+               screenText.append("3 ");
            }
        });
 
@@ -81,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("4");
+               screenText.append("4 ");
            }
        });
 
@@ -90,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("5");
+               screenText.append("5 ");
            }
        });
 
@@ -99,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("6");
+               screenText.append("6 ");
            }
        });
 
@@ -108,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("7");
+               screenText.append("7 ");
            }
        });
 
@@ -117,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("8");
+               screenText.append("8 ");
            }
        });
 
@@ -126,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("9");
+               screenText.append("9 ");
            }
        });
 
@@ -145,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
            public void onClick (View v)
            {
                if(!(screenText.length()==0))
-                   screenText.setText(screenText.getText().delete(screenText.length() - 1, screenText.length()));
+                   screenText.setText(screenText.getText().delete(screenText.length() - 2, screenText.length()));
            }
        });
 
@@ -154,7 +162,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("+");
+               screenText.append("+ ");
            }
        });
 
@@ -163,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("-");
+               screenText.append("- ");
            }
        });
 
@@ -172,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("*");
+               screenText.append("* ");
            }
        });
 
@@ -181,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append("/");
+               screenText.append("/ ");
            }
        });
 
@@ -190,8 +198,188 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick (View v)
            {
-               screenText.append(".");
+               if(!(screenText.length()==0))
+               {
+                   screenText.setText(screenText.getText().delete(screenText.length() - 1, screenText.length()));
+                   //Editable temp = (screenText.getText().delete(screenText.length() - 1, screenText.length()));
+                   //temp.append(".");
+                   //screenText.setText();
+                   String checkingLast = screenText.getText().toString();
+                   String[] lastArray = checkingLast.split("");
+                   Log.d("split0", lastArray[0]);
+                   Log.d("split-1", lastArray[-1]);
+                   if(lastArray[-1] == " "){
+                       screenText.append(" .");
+                   }
+                   else{
+                       screenText.append(".");
+                   }
+                   //screenText.append(".");
+               }
+               else{
+                   screenText.append(".");
+               }
+               //screenText.append(".");
+           }
+       });
+
+       buttonOPARENTH.setOnClickListener(new OnClickListener()
+       {
+           @Override
+           public void onClick (View v)
+           {
+               screenText.append("( ");
+           }
+       });
+
+       buttonCPARENTH.setOnClickListener(new OnClickListener()
+       {
+           @Override
+           public void onClick (View v)
+           {
+               screenText.append(") ");
+           }
+       });
+
+       buttonEQ.setOnClickListener(new OnClickListener()
+       {
+           @Override
+           public void onClick (View v)
+           {
+               String inputEquation = screenText.getText().toString();
+//               if(inputEquation.trim().length() == 0) screenText.setText("Empty");
+//               if(inputEquation.contains("*"))
+//               {
+//                   String[] temp = inputEquation.split("\\*");
+//                   Float result = Float.parseFloat(temp[0]) * Float.parseFloat(temp[1]);
+//                   String resultString = Float.toString(result);
+//                   screenText.setText(resultString);
+//               }
+//               if(inputEquation.contains("/"))
+//               {
+//                   String[] temp = inputEquation.split("/");
+//                   if(Float.parseFloat(temp[1]) == 0)
+//                   {
+//                       screenText.setText("Cannot Divide by 0");
+//                   }
+//                   else
+//                   {
+//                       Float result = Float.parseFloat(temp[0]) / Float.parseFloat(temp[1]);
+//                       String resultString = Float.toString(result);
+//                       screenText.setText(resultString);
+//                   }
+//
+//               }
+//               if(inputEquation.contains("+"))
+//               {
+//                   String[] temp = inputEquation.split("\\+");
+//                   Float result = Float.parseFloat(temp[0]) + Float.parseFloat(temp[1]);
+//                   String resultString = Float.toString(result);
+//                   screenText.setText(resultString);
+//               }
+//               if(inputEquation.contains("-"))
+//               {
+//                   String[] temp = inputEquation.split("-");
+//                   Float result = Float.parseFloat(temp[0]) - Float.parseFloat(temp[1]);
+//                   String resultString = Float.toString(result);
+//                   screenText.setText(resultString);
+//               }
+               final String ops = "-+/*";
+               StringBuilder sb = new StringBuilder();
+               Stack<Integer> s = new Stack<>();
+               for (String token : inputEquation.split("\\s")) {
+                   if (token.isEmpty())
+                       continue;
+                   char c = token.charAt(0);
+                   int idx = ops.indexOf(c);
+
+                   // check for operator
+                   if (idx != -1) {
+                       if (s.isEmpty())
+                           s.push(idx);
+
+                       else {
+                           while (!s.isEmpty()) {
+                               int prec2 = s.peek() / 2;
+                               int prec1 = idx / 2;
+                               if (prec2 > prec1 || (prec2 == prec1))
+                                   sb.append(ops.charAt(s.pop())).append(' ');
+                               else break;
+                           }
+                           s.push(idx);
+                       }
+                   }
+                   else if (c == '(') {
+                       s.push(-2); // -2 stands for '('
+                   }
+                   else if (c == ')') {
+                       // until '(' on stack, pop operators.
+                       while (s.peek() != -2)
+                           sb.append(ops.charAt(s.pop())).append(' ');
+                       s.pop();
+                   }
+                   else {
+                       sb.append(token).append(' ');
+                   }
+               }
+               while (!s.isEmpty())
+                   sb.append(ops.charAt(s.pop())).append(' ');
+               String shuntingOutput = sb.toString();
+               Log.d("shuntingOutput", shuntingOutput);
+               String finalAnswer = Double.toString(evalShunting(shuntingOutput).pop());
+               screenText.setText(finalAnswer);
+               //screenText.setText(shuntingOutput);
            }
        });
    }
+
+    public static LinkedList<Double> evalShunting(String expr)
+    {
+        String cleanExpr = cleanExpr(expr);
+        Log.d("cleanExpr", cleanExpr);
+        //String cleanExpr = expr;
+        LinkedList<Double> stack = new LinkedList<Double>();
+        //System.out.println("Input\tOperation\tStack after");
+        for(String token:cleanExpr.split("\\s")){
+            //System.out.print(token+"\t");
+            Log.d("token", token+"\t");
+            Double tokenNum = null;
+            try{
+                tokenNum = Double.parseDouble(token);
+            }catch(NumberFormatException e){}
+            if(tokenNum != null){
+                //System.out.print("Push\t\t");
+                stack.push(Double.parseDouble(token+""));
+            }else if(token.equals("*")){
+               //System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand * secondOperand);
+            }else if(token.equals("/")){
+                //System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand / secondOperand);
+            }else if(token.equals("-")){
+                //System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand - secondOperand);
+            }else if(token.equals("+")){
+                //System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand + secondOperand);
+            }else{
+                Log.d("error", stack.pop().toString());
+            }
+            //System.out.println(stack);
+        }
+        //System.out.println("Final answer: " + stack.pop());
+        return stack;
+    }
+    private static String cleanExpr(String expr){
+        //remove all non-operators, non-whitespace, and non digit chars
+        return expr.replaceAll("[^\\^\\*\\+\\-\\d/\\s]", "");
+    }
 }
